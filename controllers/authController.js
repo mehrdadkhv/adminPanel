fe8a3b95e6c3554ec117ec51e49a071dc8364cd7
mehradphp <alikhodaverdi682@gmail.com>
2022-01-12 11:07:00 +0330

    hashPass2

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,11 @@ const jwt = require('jsonwebtoken')
 const User = require('./../models/userModel')
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError')
+const signToken = id =>{
+    return jwt.sign({id},process.env.JWT_SECRET,{
+        expiresIn : process.env.JWT_EXPIRES_IN
+    })
+}
 
 exports.signup = catchAsync(async (req, res, next) => {
 
@@ -41,13 +46,18 @@ exports.login = async (req, res, next) => {
     }
     // check if user exissts and password is correct
     const user = await User.findOne({ email }).select('+password')
+
+    if(!user || !(await user.correctPassword(password,user.password))){
+        return next(new AppError('incorrect email or password ' , 401))
+    }
     console.log(user);
-    //if ok send token to client
-    const token = '';
+    //if ok send token to c lient
+    const token = signToken(user._id);
     res.status(200).json({
         status: 'success',
         token
-    })
+    })  
+     
 
 }
 
