e4e60c2e707cda36990ffed6aab5ceeb29f0650f
mehrdadphp <alikhodaverdi682@gmail.com>
2022-01-20 14:29:53 +0330

    insert slug

--- models/articleModel.js
+++ models/articleModel.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
- 
+const slugify = require('slugify') 
+
 const articleSchema = new mongoose.Schema({
 
     name: {
         type: String,
         default: [true, 'a article have a name']
     },
+    slug: String,
     title: {
         type: String,
         default: [true, 'a article have a name']
@@ -21,6 +23,22 @@ const articleSchema = new mongoose.Schema({
    
 
 })
+
+// documen middeleware run befere .save() and .create() .insertMany
+articleSchema.pre('save' , function(next) {
+    this.slug = slugify(this.name,{lower: true })
+    next()
+})
+articleSchema.pre('save', function(next) {
+    console.log('will save document...');
+    next();
+})
+articleSchema.post('save' , function(doc,next) {
+    console.log(doc);
+    next();
+})
+
+
 
 const Article = mongoose.model('Article' , articleSchema)
 
