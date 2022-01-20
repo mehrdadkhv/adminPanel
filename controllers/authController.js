dcfa1ee157ddd020a7278d63a8a571b1c1c807f9
mehrdadphp <alikhodaverdi682@gmail.com>
2022-01-20 19:31:56 +0330

    token1

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,113 +5,85 @@ const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError')
 
 
-
 const signToken = id => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
-    })
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  })
 }
 
 const createSendToken = (user, statusCode, res) => {
-    const token = signToken(user._id)
-    const cookieOptions = {
-        expires: new Date(
-            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true
+  const access_token = signToken(user._id)
+
+  const expires_in = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  }
+  if (process.env.NODE_ENV === 'production') expires_in.secure = true;
+
+  res.cookie('jwt', access_token, expires_in);
+
+  //remove pass out
+  user.password = undefined;
+  
+
+  res.status(statusCode).json({
+    status: 'success',
+    access_token,
+    data: {
+      user
     }
-    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-
-    res.cookie('jwt', token, cookieOptions);
-
-    //remove pass outPUT
-    // user.password = undefined;
-
-    res.status(statusCode).json({
-        status: 'success',
-        token,
-        data: {
-            user
-        }
-    })
+  })
 }
 
 exports.signup = catchAsync(async (req, res, next) => {
 
-    const newUser = await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        passwordconfirm: req.body.passwordconfirm
-    });
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordconfirm: req.body.passwordconfirm
+  });
 
-    const token = jwt.sign({
-        id: newUser._id
-    },
-        process.env.JWT_SECRET,
-        {
-            expiresIn: process.env.JWT_EXPIRES_IN
-        }
-    )
+  const access_token = jwt.sign({
+    id: newUser._id
+  },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN
+    }
+  )
 
-    res.status(201).json({
-        status: 'success',
-        token,
-        data: {
-            user: newUser
-        }
-    })
+  res.status(201).json({
+    status: 'success',
+    access_token,
+    data: {
+      user: newUser
+    }
+  })
 
 })
 
 exports.login = async (req, res, next) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    // 1) Check if email and password exist
-    if (!email || !password) {
-        return next(new AppError('Please provide email and password!', 400));
-    }
-    // 2) Check if user exists && password is correct
-    const user = await User.findOne({ email }).select('+password');
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({ email }).select('+password');
 
-    if (!user || !(await user.correctPassword(password, user.password))) {
-        return next(new AppError('Incorrect email or password', 401));
-    }
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
 
-    // 3) If everything ok, send token to client
-    createSendToken(user, 200, res);
+  // 3) If everything ok, send token to client
+  createSendToken(user, 200, res);
 
 
 }
 
 
 
-
-// exports.protect = catchAsync(async (req, res, next) => {
-
-//     //1 getting token and check of its there
-//     let token;
-//     if (
-//       req.headers.authorization &&
-//       req.headers.authorization.startsWith('Bearer')
-//     ) { 
-//       token = req.headers.authorization.split(' ')[1];
-//     }
-  
-//     if (!token) {
-//       return next(
-//         new AppError('You are not logged in! Please log in to get access.', 401)
-//       );
-//     }
-  
-//     // 2) Verification token
-//     const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-//      console.log(decoded);
-    
-
-//     //3  check if user still exist 
-  
-//     // 4 check if user changed pass after the  token was issued
-
-//     next()
-// })
